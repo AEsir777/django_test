35e4061cd566c601f2fecc298b580d23b42852df
AEsir777 <lkb2438834817@gmail.com>
2022-10-24 18:33:57 -0400

    add delete and update feature

--- app2/urls.py
+++ app2/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('app2', views.App2ListView.as_view(), name = "file.list"),
     path('app2/<int:pk>', views.App2DetailView.as_view(), name="detail"),
-    path('app2/upload', views.App2CreateView.as_view(), name = "upload.file")
+    path('app2/upload', views.App2CreateView.as_view(), name = "upload.file"),
+    path('app2/<int:pk>/update', views.App2UpdateView.as_view(), name = 'update.file'),
+    path('app2/<int:pk>/delete', views.App2DeleteView.as_view(), name = 'update.delete')
 ]
--- app2/views.py
+++ app2/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from app2.forms import FileForm
 from .models import App2
+
+class App2UpdateView(UpdateView):
+    model = App2
+    form_class = FileForm
+    success_url = '/app2'
+    template_name = "app2/upload_file.html"
 
 class App2CreateView(CreateView):
     model = App2
     form_class = FileForm
     success_url = '/app2'
     template_name = "app2/upload_file.html"
+
+class App2DeleteView(DeleteView):
+    model = App2
+    context_object_name = 'file'
+    success_url = '/app2'
+    template_name = "app2/delete.html"
 
 class App2ListView(ListView):
     model = App2
